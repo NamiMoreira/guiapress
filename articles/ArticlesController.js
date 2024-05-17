11d922bc0b48a4fd88905321f2cf0a61a898294e
Naomi Moreira <149202489+NamiMoreira@users.noreply.github.com>
2024-05-17 11:36:00 -0300

    Inicio do model de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,16 @@
 const express = require ('express')
 const router = express.Router();
+const Category = require('../categories/Category');
 
 router.get('/articles',(req,res) => {
     res.send('rota de articles')
 });
 
 router.get('/admin/articles/new',(req,res) => {
-    res.send('rota para criar uma nova articles')
+    Category.findAll().then(categories =>{
+        res.render('admin/articles/new.ejs',{categories: categories})
+    })
+    
 });
 
 module.exports = router;
